bc167966fa356980eece963f67d65cb0c9a5b0e1
willen <willen.sesantos@gmail.com>
2023-06-23 19:10:56 -0300

    Project hw-payroll created

---
+++ hrpayroll/src/main/java/com/willen/hrpayroll/HrpayrollApplication.java
@@ -0,0 +1,13 @@
+package com.willen.hrpayroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrpayrollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrpayrollApplication.class, args);
+	}
+
+}
---
+++ hrpayroll/src/test/java/com/willen/hrpayroll/HrpayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willen.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrpayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
