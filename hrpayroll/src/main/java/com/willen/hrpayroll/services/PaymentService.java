c52d909881142f757e7166bcd5884cd0bd8ecf39
willen <willen.sesantos@gmail.com>
2023-06-24 17:28:30 -0300

    RestTemplate

--- hrpayroll/src/main/java/com/willen/hrpayroll/services/PaymentService.java
+++ hrpayroll/src/main/java/com/willen/hrpayroll/services/PaymentService.java
@@ -1,15 +1,31 @@
 package com.willen.hrpayroll.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.willen.hrpayroll.entities.Payment;
+import com.willen.hrpayroll.entities.Worker;
 
 @Service
 public class PaymentService {
 
-    public Payment getPayment(long workerId, int days) {
-        
-        // mocked payment
-        return new Payment("Bob", 200.00, days);
+    @Value("${hr-worker.host}")
+    private String workerHost;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public Payment getPayment(Long workerId, int days) {
+
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("id", "" + workerId);
+
+        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
