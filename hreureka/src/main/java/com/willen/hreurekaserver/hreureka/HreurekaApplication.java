65f15e8f1c2b07c1b605dceb9d59abf84358b65f
willen <willen.sesantos@gmail.com>
2023-06-25 16:55:38 -0300

    Project hr-eureka-server created

---
+++ hreureka/src/main/java/com/willen/hreurekaserver/hreureka/HreurekaApplication.java
@@ -0,0 +1,13 @@
+package com.willen.hreurekaserver.hreureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HreurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HreurekaApplication.class, args);
+	}
+
+}
---
+++ hreureka/src/test/java/com/willen/hreurekaserver/hreureka/HreurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willen.hreurekaserver.hreureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HreurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
