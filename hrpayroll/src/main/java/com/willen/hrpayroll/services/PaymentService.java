fce4309224a75abe52e5d2b3c76ba01918e83351
willen <willen.sesantos@gmail.com>
2023-06-24 22:42:36 -0300

    Feign Client

--- hrpayroll/src/main/java/com/willen/hrpayroll/services/PaymentService.java
+++ hrpayroll/src/main/java/com/willen/hrpayroll/services/PaymentService.java
@@ -1,31 +1,21 @@
 package com.willen.hrpayroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.willen.hrpayroll.entities.Payment;
 import com.willen.hrpayroll.entities.Worker;
+import com.willen.hrpayroll.feignClients.WorkerFeign;
 
 @Service
 public class PaymentService {
 
-    @Value("${hr-worker.host}")
-    private String workerHost;
-
     @Autowired
-    private RestTemplate restTemplate;
+    private WorkerFeign workerFeignClient;
 
     public Payment getPayment(Long workerId, int days) {
 
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("id", "" + workerId);
-
-        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+        Worker worker = workerFeignClient.findById(workerId).getBody();
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
