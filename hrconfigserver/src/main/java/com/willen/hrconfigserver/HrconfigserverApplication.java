0118c004731cb5724f9d9fc90fdd051d2af12b7c
willen <willen.sesantos@gmail.com>
2023-06-30 16:09:47 -0300

    Project hr-config-server-created

---
+++ hrconfigserver/src/main/java/com/willen/hrconfigserver/HrconfigserverApplication.java
@@ -0,0 +1,13 @@
+package com.willen.hrconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrconfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrconfigserverApplication.class, args);
+	}
+
+}
---
+++ hrconfigserver/src/test/java/com/willen/hrconfigserver/HrconfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willen.hrconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrconfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
