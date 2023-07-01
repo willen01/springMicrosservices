a44cbd270e39d21cbe8f4d5893c9f32d2c0d79d8
willen <willen.sesantos@gmail.com>
2023-07-01 19:19:41 -0300

    Private gitlab Repository

--- hrconfigserver/src/main/java/com/willen/hrconfigserver/HrconfigserverApplication.java
+++ hrconfigserver/src/main/java/com/willen/hrconfigserver/HrconfigserverApplication.java
@@ -1,15 +1,25 @@
 package com.willen.hrconfigserver;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-public class HrconfigserverApplication {
+public class HrconfigserverApplication implements CommandLineRunner {
+	// CommandLineRunner - executa comandos durante inicialização do projeto
+
+	@Value("${spring.cloud.config.server.git.username}")
+	private String username;
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrconfigserverApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+//		System.out.println("USERNAME = " + username);
+	}
 }
