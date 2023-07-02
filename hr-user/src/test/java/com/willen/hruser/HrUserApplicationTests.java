2a941e1ca394935cfd578699a18af86b550440ad
willen <willen.sesantos@gmail.com>
2023-07-01 21:33:51 -0300

    Project hr-user created

---
+++ hr-user/src/test/java/com/willen/hruser/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willen.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
